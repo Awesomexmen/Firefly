0b10af79c47b974ff1241f84b2320dd2c4c33ec7
Tomer Singal <44323097+Singalt16@users.noreply.github.com>
2019-02-23 13:40:36 -0500

    splitting classes into files

--- HackCONJ.java
+++ HackCONJ.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
+
 public class HackCONJ 
 {
 	public static void main (String[] args) 
@@ -8,90 +9,3 @@ public class HackCONJ
 	    
 	}
 }
-class swarm
-{
-    public swarm(double r, int n)
-    {
-        
-    }
-}
-class fifly
-{
-    private int xco;
-    private int yco;
-    public boolean canflash;
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-//construct a firefly!
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    
-    public fifly(int x, int y)
-    {
-        xco = x;
-        yco = y;
-        canflash = true;
-    }
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-//flashin
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    public void flash()
-    {
-        if (canflash)
-        {
-            //flash on the thing
-            isflash = true;
-            this.emit;
-            this.cooldown;
-        }
-    }
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-//getters
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    public int getx()
-    {
-        return xco;
-    }
-    
-    public int gety()
-    {
-        return yco;
-    }
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-//cooldown son!
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    public void cooldown()
-    {
-        double instant = Clock.millis();
-        while (instant - Clock)
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
