a66eb400b2063a0cdd6c4b5df6eab058ceb651bd
Tomer Singal <44323097+Singalt16@users.noreply.github.com>
2019-02-23 13:49:47 -0500

    Update Swarm.java

--- Swarm.java
+++ Swarm.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class Swarm
 {
-    public swarm(double r, int n)
-    {
-        
+    ArrayList<Firefly> fireflies;
+    public Swarm(int n) {
+        for (int i = 0; i < n; i++) {
+            fireflies.add(new Firefly(/*some x*/, /*some y*/));
+        }
     }
 }
