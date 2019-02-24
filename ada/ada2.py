6943157bcc647b0c36a36626c7794a89898c2fc6
ethanzeigler <ethanzeigler@gmail.com>
2019-02-23 22:38:18 -0500

    actually grid

--- ada/ada2.py
+++ ada/ada2.py
@@ -7,30 +7,45 @@ pixel_pin = board.D18
 num_pixels = 30
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.03, auto_write=False, pixel_order=ORDER)
-meta_pixels = [[0, 0, 0, 0] for _ in range(len(pixels))]
 
-up_to = 0
+mtx_mp = {
+    (0,0): 0,
+    (0,1): 1,
+    (0,2): 2,
+    (0,3): 3,
+    (0,4): 4,
+    (0,5): 5,
+    (1,5): 9,
+    (1,4): 10,
+    (1,3): 11,
+    (1,2): 12,
+    (1,1): 13,
+    (1,0): 14,
+    (2,0): 18,
+    (2,1): 19,
+    (2,2): 20,
+    (2,3): 21,
+    (2,4): 22,
+    (2,5): 23,
+    (3,5): 27,
+    (3,4): 28,
+    (3,3): 29,
+    (3,2): 30,
+    (3,1): 31,
+    (3,0): 32,
+    (4,0): 36,
+    (4,1): 37,
+    (4,2): 38,
+    (4,3): 39,
+    (4,4): 40,
+    (4,5): 41,
+    (5,5): 45,
+    (5,4): 46,
+    (5,3): 47,
+    (5,2): 48,
+    (5,1): 49,
+    (5,0): 50
+}
 
-for up_to in range(len(pixels)):
-    for _ in range(255):
-        for bulb in range(up_to):
-            stage = meta_pixels[bulb][3]
-            if stage < 255:
-                meta_pixels[bulb][0] += 1
-            elif stage < 255 * 2:
-                meta_pixels[bulb][0] -= 1
-            elif stage < 255 * 3:
-                meta_pixels[bulb][1] += 1
-            elif stage < 255 * 4:
-                meta_pixels[bulb][1] -= 1
-            elif stage < 255 * 5:
-                meta_pixels[bulb][2] += 1
-            elif stage < 255 * 6:
-                meta_pixels[bulb][2] -= 1
-            else:
-                meta_pixels[bulb] = [0,0,0,0]
-                continue
-            pixels[bulb] = (meta_pixels[bulb][0], meta_pixels[bulb][1], meta_pixels[bulb][2])
-            meta_pixels[bulb][3] += 1
-        pixels.show()
+
 
